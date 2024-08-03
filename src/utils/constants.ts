5fbdebfadf1b8601e35603c77bc305c63dc26cd2
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-03 19:07:48 -0400

    feat: make scopes more adjustable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,13 @@ if (!spotifyClientID) {
 export const spotify = {
   AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
   CLIENT_ID: spotifyClientID,
-  SCOPES:
-    "playlist-modify-public playlist-read-private user-library-read user-read-private user-read-email playlist-read-collaborative",
+  SCOPES: [
+    "playlist-modify-public",
+    "playlist-read-private",
+    "user-library-read",
+    "user-read-private",
+    "user-read-email",
+    "playlist-read-collaborative",
+  ].join(" "),
   REDIRECT_URI: "http://localhost:3000/callback/",
 };
