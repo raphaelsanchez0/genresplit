58c840b43135040ec99a1e5b1778a5bd787044d7
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-04 05:22:12 -0400

    style: search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,10 @@
+import SearchPlaylists from "@/components/search-playlists/SearchPlaylists";
 import React from "react";
 
 export default function Page() {
-  return <div className="page"></div>;
+  return (
+    <div className="page">
+      <SearchPlaylists />
+    </div>
+  );
 }
--- src/components/search-playlists/SearchPlaylists.tsx
+++ src/components/search-playlists/SearchPlaylists.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import SearchBar from "./search-bar/SearchBar";
 
 export default function SearchPlaylists() {
-  return <div>SearchPlaylists</div>;
+  return (
+    <Card className="h-full w-3/4">
+      <div className="flex flex-col items-center justify-center p-6">
+        <CardTitle className="text-5xl text-stone-950">
+          Search Playlists
+        </CardTitle>
+        <SearchBar />
+        <CardDescription>
+          Search and select playlists for splitting. Whatever you choose will be
+          aggregated for you to split into playlists of new genres
+        </CardDescription>
+      </div>
+    </Card>
+  );
 }
