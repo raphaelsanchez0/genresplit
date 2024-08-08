24613820ad0454b48a2202b7c1dece6dc3ad600d
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-08 19:02:42 -0400

    SearchPlaylists now uses client

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,12 @@
 import SearchPlaylists from "@/components/search-playlists/SearchPlaylists";
+import { getSpotifyToken } from "@/utils/authHelpers";
 import React from "react";
 
-export default function Page() {
+export default function Search() {
+  const token = getSpotifyToken();
   return (
     <div className="page">
-      <SearchPlaylists />
+      <SearchPlaylists token={token} />
     </div>
   );
 }
--- src/components/search-playlists/SearchPlaylists.tsx
+++ src/components/search-playlists/SearchPlaylists.tsx
@@ -1,18 +1,17 @@
+"use client";
 import React from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import SearchBar from "./search-bar/SearchBar";
 import PlaylistList from "./playlists-list/PlaylistList";
 import { getSpotifyToken } from "@/utils/authHelpers";
 
-export default function SearchPlaylists() {
-  const token = getSpotifyToken();
+export default function SearchPlaylists({ token }: { token: string }) {
   return (
     <Card className="h-full w-3/4">
       <div className="flex flex-col items-center justify-center p-6 gap-4">
         <CardTitle className="text-5xl text-stone-950">
           Search Your Playlists
         </CardTitle>
-
         <SearchBar />
         <CardDescription className="text-md text-stone-950">
           Select one or more playlists to pick genres from
