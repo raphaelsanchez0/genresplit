331a4c699387a64c35461841314cecde1c69d7f6
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-11 15:44:49 -0400

    feat: move SelectGenres logic into useSortedGenres

--- src/components/select-genres/SelectGenres.tsx
+++ src/components/select-genres/SelectGenres.tsx
@@ -15,60 +15,20 @@ import { getGenreFrequencyAmongArtists } from "@/utils/api/genres/genres";
 import { GenreFrequency, PlaylistTrackObjectWithGenres } from "@/utils/types";
 import GenresList from "./genres-list/GenresList";
 import useSearchParamGenres from "@/hooks/useSearchParamGenres";
+import { Button } from "../ui/button";
+import useSortedGenres from "./useSortedGenres";
 
 export default function SelectGenres({ token }: { token: string }) {
   const searchParams = useSearchParams();
   const selectedPlaylists = useSearchParamPlaylists({ searchParams });
-  const router = useRouter();
 
-  const [artists, setArtists] = useState<Map<string, number>>(new Map());
-  const [sortedGenres, setSortedGenres] = useState<GenreFrequency[]>([]);
+  const sortedGenres = useSortedGenres(selectedPlaylists, token);
 
-  const selectedGenres = useSearchParamGenres(searchParams);
-
-  useEffect(() => {
-    const addGenresToTracks = async () => {
-      //Key: artistID, Value: Genres[]
-      const artistGenres = new Map<string, string[]>();
-      const genreCount = new Map<string, number>();
-      const allTracksWithGenres = new Set<PlaylistTrackObjectWithGenres>();
-
-      const allTracksWithoutGenres = await getPlaylistTracksTracksInPlaylists(
-        selectedPlaylists,
-        token
-      );
-
-      for await (const track of allTracksWithoutGenres) {
-        const artistID = track.track?.artists[0]?.id!;
-        let genres: string[] | undefined = artistGenres.get(artistID);
-        if (!genres) {
-          genres = await getArtistGenres(artistID, token);
-          artistGenres.set(artistID, genres);
-        }
-
-        for (const genre of genres) {
-          const currentCount = genreCount.get(genre) || 0;
-          genreCount.set(genre, currentCount + 1);
-        }
-
-        allTracksWithGenres.add({
-          ...track,
-          genres: genres,
-        });
-      }
-
-      const genresArray = Array.from(genreCount);
-      const sortedGenresArray: GenreFrequency[] = genresArray
-        .sort((a, b) => b[1] - a[1])
-        .map(([genre, frequency]) => ({ genre, frequency }));
-
-      console.log(sortedGenresArray);
-
-      setSortedGenres(sortedGenresArray);
-    };
-
-    addGenresToTracks();
-  }, []);
+  const selectedGenresParam = searchParams.get("selectedGenres");
+  const atLeastOneGenreSelected = !!selectedGenresParam;
+  const handleSubmit = () => {
+    console.log(sortedGenres);
+  };
   return (
     <Card className="full-page-card">
       <div className="grid grid-cols-3">
@@ -79,6 +39,11 @@ export default function SelectGenres({ token }: { token: string }) {
           <CardDescription className="text-md text-stone-950 text-center">
             Select the genres you want to include in your new playlist
           </CardDescription>
+        </div>
+        <div className="flex items-center justify-center">
+          <Button disabled={!atLeastOneGenreSelected} onClick={handleSubmit}>
+            Get Genres
+          </Button>
         </div>
       </div>
       <GenresList genres={sortedGenres} token={token} />
---
+++ src/components/select-genres/useSortedGenres.tsx
@@ -0,0 +1,55 @@
+import { getArtistGenres } from "@/utils/api/artists/artists";
+import { getPlaylistTracksTracksInPlaylists } from "@/utils/api/playlists/playlist";
+import { GenreFrequency, PlaylistTrackObjectWithGenres } from "@/utils/types";
+import { useEffect, useState } from "react";
+
+export default function useSortedGenres(
+  selectedPlaylists: Set<string>,
+  token: string
+) {
+  const [sortedGenres, setSortedGenres] = useState<GenreFrequency[]>([]);
+  useEffect(() => {
+    const addGenresToTracks = async () => {
+      //Key: artistID, Value: Genres[]
+      const artistGenres = new Map<string, string[]>();
+      const genreCount = new Map<string, number>();
+      const allTracksWithGenres = new Set<PlaylistTrackObjectWithGenres>();
+
+      const allTracksWithoutGenres = await getPlaylistTracksTracksInPlaylists(
+        selectedPlaylists,
+        token
+      );
+
+      for await (const track of allTracksWithoutGenres) {
+        const artistID = track.track?.artists[0]?.id!;
+        let genres: string[] | undefined = artistGenres.get(artistID);
+        if (!genres) {
+          genres = await getArtistGenres(artistID, token);
+          artistGenres.set(artistID, genres);
+        }
+
+        for (const genre of genres) {
+          const currentCount = genreCount.get(genre) || 0;
+          genreCount.set(genre, currentCount + 1);
+        }
+
+        allTracksWithGenres.add({
+          ...track,
+          genres: genres,
+        });
+      }
+
+      const genresArray = Array.from(genreCount);
+      const sortedGenresArray: GenreFrequency[] = genresArray
+        .sort((a, b) => b[1] - a[1])
+        .map(([genre, frequency]) => ({ genre, frequency }));
+
+      console.log(sortedGenresArray);
+
+      setSortedGenres(sortedGenresArray);
+    };
+
+    addGenresToTracks();
+  }, []);
+  return sortedGenres;
+}
