07388dc843aa3f01ad83d9be32fd652b09370deb
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-13 02:48:21 -0400

    fix: middleware no longer async

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { createAuthURL } from "@/utils/authHelpers";
 import { cookieNames } from "@/utils/constants";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get(cookieNames.SPOTIFY_TOKEN)?.value;
 
   if (!currentUser) {
