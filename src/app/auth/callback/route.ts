4ebf62264dabc87917f5de710d9ec38d0be36440
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-14 18:14:41 -0400

    feat: add comments to check where redirect is going

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -34,6 +34,6 @@ export async function GET(request: Request) {
   }
 
   // Redirect to an error page if the code is not present
-  const errorUrl = new URL("/auth/auth-code-error", origin);
+  const errorUrl = new URL("/auth/auth-code-error/nocode", origin);
   return NextResponse.redirect(errorUrl.toString());
 }
