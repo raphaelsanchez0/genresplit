66c5ccb63a95abc8f664a573b77eb25b8cf49df1
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-18 15:33:11 -0400

    feat: landing page is responsive

--- src/components/navbar/NavbarLanding.tsx
+++ src/components/navbar/NavbarLanding.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function NavbarLanding() {
   return (
-    <div className="w-full bg-secondary-color flex items-center py-4 max-w-5xl">
+    <div className="w-full bg-secondary-color flex items-center py-4 max-w-5xl px-4 md:px-0">
       <h1 className="text-accent-color mr-auto">
         <Link href="/">GenreSplit</Link>
       </h1>
