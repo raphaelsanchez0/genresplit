0d972cef4bd60a593e9c1acfead398dc8af29458
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-18 15:54:55 -0400

    feat: navbar is fully responsive

--- src/components/navbar/NavbarUI.tsx
+++ src/components/navbar/NavbarUI.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function NavbarUI() {
   return (
     <div className="w-full bg-secondary-color p-4 justify-center">
-      <div className="flex w-3/4 items-center mx-auto justify-between w-full">
+      <div className="flex md:w-3/4 items-center mx-auto justify-between w-full">
         <h1 className="text-accent-color mr-auto">
           <Link href="/">GenreSplit</Link>
         </h1>
